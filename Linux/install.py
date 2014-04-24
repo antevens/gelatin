c8981be5486baf4f4761d634b2e9836ccb9aac4d
Jarl Stefansson <jarl.stefansson@gmail.com>
2014-04-24 14:43:28 -0400

    Linux installer runs

--- Linux/install.py
+++ Linux/install.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 import shutil
 from datetime import date
-import time
 import os
 from xml.etree import ElementTree
 from subprocess import call
 
 symbol_dirs = ['/usr/share/X11/xkb/symbols', '/etc/X11/xkb/symbols']
 rules_dir = '/usr/share/X11/xkb/rules'
+
 
 def add_layout():
     for symbol_dir in symbol_dirs:
@@ -22,13 +22,15 @@ def add_layout():
                 else:
                     first.extend(data)
             if first is not None:
-                with open(os.path.join(symbol_dir, 'evdev.xml'), "w") as f:
+                with open(os.path.join(rules_dir, 'evdev.xml'), "w") as f:
                     f.write(ElementTree.tostring(first))  # write the new line before
 
 
 def switch_layout():
-    if call("setxkbmap -v us -variant gelatin"):
+    if call(['setxkbmap', '-v', 'us', '-variant', 'gelatin']):
         print 'Successfully set keyboard to Gelatin'
+    else:
+        print 'Failed to set keyboard to Gelatin'
 
 if __name__ == "__main__":
     add_layout()
